adfaac5f7151093f4c9e05ae1d53719ee19661e8
mr4jay <rajaykumar2222@gmail.com>
2023-07-18 17:52:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-function toggleDetails(phaseId) {
-  const phaseDetails = document.getElementById(`phase${phaseId}-details`);
-  const currentDisplay = phaseDetails.style.display;
-  const allPhaseDetails = document.querySelectorAll(".phase-details");
+function toggleDetails(index) {
+  const details = document.querySelectorAll(".phase-details");
+  details.forEach((item) => {
+    item.style.display = "none";
+  });
 
-  allPhaseDetails.forEach((details) => (details.style.display = "none"));
-
-  if (currentDisplay === "none" || currentDisplay === "") {
-    phaseDetails.style.display = "block";
-    typeDocumentationSteps(phaseDetails);
-  }
+  const selectedPhase = document.getElementById(`phase${index}-details`);
+  selectedPhase.style.display = "block";
 }
-
-function typeDocumentationSteps(phaseDetails) {
-  const documentationSteps = phaseDetails.querySelectorAll("ol li");
-  let stepIndex = 0;
-
-  function typeStep() {
-    if (stepIndex < documentationSteps.length) {
-      documentationSteps[stepIndex].style.display = "inline";
-      stepIndex++;
-      setTimeout(typeStep, 150); // Adjust typing speed here
-    }
-  }
-
-  typeStep();
-}
-
-// Add event listeners to each phase tile
-const projectPhases = document.querySelector(".project-phases");
-const phaseTiles = projectPhases.querySelectorAll(".project-phase");
-phaseTiles.forEach((phaseTile, index) => {
-  phaseTile.addEventListener("click", () => toggleDetails(index + 1));
-});
