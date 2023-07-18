ed94977189e85040c9b0c762d177c7952174ef21
mr4jay <rajaykumar2222@gmail.com>
2023-07-18 18:28:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-function toggleDetails(phaseNumber) {
-  const phaseDetails = document.getElementById(`phase${phaseNumber}-details`);
-  phaseDetails.classList.toggle("active");
+function saveData(phase) {
+  const form = document.querySelector(`.${phase}-form`);
+  const formData = new FormData(form);
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
+
+  // Replace the URL with your server endpoint for saving data
+  fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  .then(response => response.json())
+  .then(result => {
+    alert(`Data saved for ${phase}`);
+    form.reset();
+  })
+  .catch(error => {
+    console.error('Error saving data:', error);
+  });
 }
